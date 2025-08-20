00a0e849df179f3b81439be60079e6a67b83ff82
Selfi <celpi.irawan@gmail.com>
2025-08-20 19:54:29 +0800

    passing and using data to and from home.html

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,23 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'myapp/home.html', {})
+
+    # Steps:
+        # (in future) 1 - get data from database
+
+    # 2 - give data to html
+    context = {
+        "name": "Alice",
+        "age": 30,
+        "phone": "0123456",
+        "address": "KL",
+
+        "subjects": ["English", "Math", "Coding", "History", "Geo", "Art", "Sport"]
+    }
+
+    # 3 (go to home.html) - use data on html
+
+    return render(request, 'myapp/home.html', context)
 
 def about(request):
     return render(request, 'myapp/about.html', {})
