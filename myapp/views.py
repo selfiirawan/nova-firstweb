f14ff6a27aa6ea4e118e34c9caa53d4bc04d8d82
Selfi <celpi.irawan@gmail.com>
2025-08-25 19:39:24 +0800

    update last week class

--- myapp/views.py
+++ myapp/views.py
@@ -13,8 +13,29 @@ def home(request):
         "age": 30,
         "phone": "0123456",
         "address": "KL",
+        "is_graduated": True,
 
-        "subjects": ["English", "Math", "Coding", "History", "Geo", "Art", "Sport"]
+        "subjects": ["English", "Math", "Coding", "History", "Geo", "Art", "Sport"],
+        "books": [
+            {
+                "title": 'The Python Cookbook',
+                "author": "David Beazley",
+                "pages": 500,
+                "read": False,
+            },
+            {
+                "title": "Automate the Boring Stuff",
+                "author": "Al Sweigart",
+                "pages": 400,
+                "read": True,
+            },
+            {
+                'title': 'Fluent Python',
+                'author': 'Luciano Ramalho',
+                'pages': 800,
+                'read': False,
+            },
+        ],
     }
 
     # 3 (go to home.html) - use data on html
